75d2e7f8a931d7da813a1ff75653a9101a297d72
Milana <milanaknezevicc98@gmail.com>
2024-04-09 19:03:51 +0200

    done

--- validation/validation.js
+++ validation/validation.js
@@ -25,7 +25,7 @@ const registrationSchema = yup.object().shape({
 const updateUserSchema = yup.object().shape({
     name: yup.string().required('Name is required!'),
     lastname: yup.string().required('Lastname is required!'),
-    mail: yup.string().email('Enter a valid email address').required('Email is required!'),
+    email: yup.string().email('Enter a valid email address').required('Email is required!'),
     username: yup.string().required('Username is required!'),
     phoneNumber: yup.string().required('Phone number is required!'),
     role: yup.string().required('Role is required!'),
